37a6a66310a7ac8a6bd1c6f023312f5d0ae2b2e5
DaoNguyen <dao.nbm1502@gmail.com>
2017-01-01 18:35:57 +0700

    Lesson1_Variable_Type_Operations
    Acquainted with Variables, Type and Operations in Java language

---
+++ Perimeter_Area_Rectangles_Circles/gen/com/dao/perimeter_area_rectangles_circles/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.dao.perimeter_area_rectangles_circles;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
